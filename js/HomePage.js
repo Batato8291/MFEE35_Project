07cf724fec9822ce8e4b917e94f0d92fa8c5d66e
AAAwawa <batato4ni@gmail.com>
2023-02-08 11:56:22 +0800

    git add . git commit -m 'navbar finish' git push

--- js/HomePage.js
+++ js/HomePage.js
@@ -1,25 +1,19 @@
-const homeNavbar = document.querySelector("#navbar");
+const homeNavbar = document.querySelector("#header");
 let lastPos = 0;
 
 document.addEventListener("scroll", function () {
   let currentPos = window.scrollY;
-  // 手下滑
+  const selectList = document.querySelector("#slide_selects");
+  const selectBtn = document.querySelector("#select_btn");
+  // 頁面下滑
   if (currentPos > lastPos) {
+    selectList.classList.remove("show");
+    selectBtn.ariaExpanded = false;
     homeNavbar.classList.add("hide");
   } else {
-    // 手上滑
+    // 頁面上滑
     homeNavbar.classList.remove("hide");
   }
   // 根據未來位置做比較
   lastPos = currentPos;
-});
-
-window.addEventListener("scroll", function () {
-  let target = document.querySelector(".slide_item");
-  let targetPos = target.getBoundingClientRect().top;
-  let windowHeight = window.innerHeight;
-
-  if (targetPos < windowHeight) {
-    target.classList.add("slide_in");
-  }
 });
